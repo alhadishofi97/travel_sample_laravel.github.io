15408a8591daad342f03ab9352fdc83fe584d649
alhadishofi97 <shofialhadi@gmail.com>
2022-04-14 00:19:56 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    @include('includes.style')
+    @stack('addon-style')
+    @stack('prepend-style')
+</head>
+
+<body>
+    @include('includes.navbar')
+    @yield('content')
+    @include('includes.footer')
+
+    @stack('prepend-script')
+    @include('includes.script')
+    @stack('addon-script')
+
+
+</body>
+
+</html>
